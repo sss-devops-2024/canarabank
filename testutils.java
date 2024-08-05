c73f212e5d1252746158256157cc2e17f03d9497
Sairaviteja999 <sairavibhimavarapu999@gmail.com>
2024-08-05 15:53:07 +0000

    testutils.java

---
+++ testutils.java
@@ -0,0 +1,4 @@
+echo "This file related to the test content file"
+echo "presently iam using the vim editor"
+echo "Iam sairaviteja new to this team"
+echo "welcome to my shellecript..."
---
+++ warutilites.java
@@ -0,0 +1,8 @@
+echo "Welcome to my script"
+echo "iam using the vim editor rightnow.."
+echo "this file related to the war files..."
+echo "The list of the filecontents..."
+ls
+echo "Today date is:"
+date
+
