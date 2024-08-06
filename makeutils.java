86d61fd228fd62dd06311df5a1b9233cad94027b
Sairaviteja999 <sairavibhimavarapu999@gmail.com>
2024-08-06 01:09:55 +0000

    Revert "fourth commit" "reveted " This reverts commit a9694c6e275f30147dc87b45bf77f79e753b1ecb.

--- makeutils.java
+++
@@ -1,4 +0,0 @@
-echo "Welcome to my shellscript"
-echo "Iam using the vim editor rightnow.."
-echo "This file related to the maven files.."
-echo "related to the shellscript.."
--- shutils.java
+++
@@ -1,4 +0,0 @@
-echo "Helloeveryone"
-echo "iam sairaviteja"
-echo "Iam using this vim editor right now"
-echo "iam using the file related to the file java"
